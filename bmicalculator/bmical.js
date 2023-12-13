cfd74b6323eea953908d0cc3368c45243de39948
AlanRoy25 <alanroythomas4@gmail.com>
2023-12-14 03:07:58 +0530

    bmi cal

---
+++ bmicalculator/bmical.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('.container form');
+//this use case will give us empty value if we write this outside the event listener
+//const Height = parseInt(document.querySelector("#height"))
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+
+const heightInput = document.querySelector("#height").value;
+const weightInput = document.querySelector("#weight").value;
+const results = document.querySelector('#result');
+
+const Height = parseInt(heightInput, 10);
+const Weight = parseInt(weightInput, 10);
+
+if(isNaN(Height) || Height <= 0){
+  results.innerHTML = `Please enter a Valid Height! `
+  return;
+}
+else if (isNaN(Weight) || Weight <= 0 ){
+  results.innerHTML = `Please enter a Valid Weight! `
+  return;
+}
+else {
+ const bmi =  (Weight / ((Height/100)*(Height/100))).toFixed(2);
+results.innerHTML = `<span>${bmi}</span>`
+}
+});
